7f3e73aba435a30ff1d23dd36a0690e9780f65db
sahaiash <sahaiashutoshvinayak@gmail.com>
2025-07-15 17:53:40 +0530

    Background Effects

---
+++ src/pages/home.jsx
@@ -0,0 +1,17 @@
+export const Home = () => {
+    return(
+        <div className="min-y-screen">
+           {/* theme toggle */}
+
+
+           {/* background effect */}
+             
+
+           {/* Navbar */}
+
+           {/* Main content */}
+
+           {/* Footer */}
+        </div>
+    )
+}
