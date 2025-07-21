62f3dbdadc23630edda64b1208e7602ae6816a7a
sahaiash <sahaiashutoshvinayak@gmail.com>
2025-07-21 17:58:45 +0530

    Resuming

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom"
 import {Home} from "./pages/home"
 import NotFound from "./pages/NotFound";
+import { ThemeToggle } from "./components/ThemeToggle";
+
 function App(){
   return(
-    <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    </BrowserRouter>
-    </>
+    <div className="relative min-h-screen">
+      <div className="absolute top-4 right-4 z-50">
+        <ThemeToggle />
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 export default App
